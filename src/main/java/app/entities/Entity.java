75ae89326c8731b14650a8758b8e5511926213b5
Oleg <o.yevstratyev@gmail.com>
2020-01-16 15:14:03 +0300

    init-commit_draft_hibernate_spring_MVC

--- src/main/java/app/entities/Entity.java
+++
@@ -1,6 +0,0 @@
-package app.entities;
-
-import java.io.Serializable;
-
-public abstract class Entity implements Serializable, Cloneable {
-}
--- src/main/java/app/entities/enums/VisaType.java
+++
@@ -1,8 +0,0 @@
-package app.entities.enums;
-
-public enum VisaType {
-    A,
-    B,
-    C,
-    D
-}
